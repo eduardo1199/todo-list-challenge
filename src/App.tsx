c975112bab919086e5464d53f83e652cb49fbe0c
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-02-21 21:50:49 -0300

    add eslint and focus styled input and button

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,15 @@ export function App() {
       <div className="flex w-full justify-center">
         <div className="w-[736px] ">
           <form className="mt-[-27px] flex gap-2">
-            <input 
-              type="text" 
-              className="bg-gray-500 rounded-lg w-[638px] p-4 h-[54px] border border-gray-700" 
-              placeholder="Adicione uma nova tarefa" 
+            <input
+              type="text"
+              className="bg-gray-500 rounded-lg w-[638px] p-4 h-[54px] border border-gray-700 text-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-dark"
+              placeholder="Adicione uma nova tarefa"
             />
-            <button type="button" className="flex flex-1 h-[52px] justify-center items-center gap-8 rounded-lg bg-blue-dark hover:bg-blue transition-colors">
+            <button
+              type="button"
+              className="flex flex-1 h-[52px] justify-center items-center gap-8 rounded-lg focus:ring-purple-dark focus:ring-2 bg-blue-dark focus:outline-none hover:bg-blue transition-colors"
+            >
               <p className="text-gray-200 font-bold text-sm">Criar</p>
             </button>
           </form>
