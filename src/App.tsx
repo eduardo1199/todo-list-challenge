d307ae634059062776e6010358ac80b2ff0831da
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-02-27 21:31:13 -0300

    created border

--- src/App.tsx
+++ src/App.tsx
@@ -41,8 +41,8 @@ export function App() {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col justify-center items-center mt-16">
-              <Clipboard size={56} className="text-gray-400" />
+            <div className="flex flex-col justify-center items-center border-t border-gray-300 rounded-t-lg rounded-r-lg">
+              <Clipboard size={56} className="text-gray-400 mt-16" />
               <strong className="block mt-4 font-bold text-base text-gray-300">
                 Você ainda não tem tarefas cadastradas
               </strong>
