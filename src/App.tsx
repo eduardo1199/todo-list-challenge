16dacc277cdf1b7421c57c2f70bb0f9c803f64b4
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-01 18:04:57 -0300

    created component form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import { Clipboard } from 'phosphor-react'
 import * as Checkbox from '@radix-ui/react-checkbox'
 import { Check, Trash } from 'phosphor-react'
+import { Form } from './components/Form'
 import { Header } from './components/Header'
 
 export function App() {
@@ -9,19 +10,7 @@ export function App() {
       <Header />
       <div className="flex w-full justify-center">
         <div className="w-[736px]">
-          <form className="mt-[-27px] flex gap-2">
-            <input
-              type="text"
-              className="bg-gray-500 rounded-lg w-[638px] p-4 h-[54px] border border-gray-700 text-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-dark"
-              placeholder="Adicione uma nova tarefa"
-            />
-            <button
-              type="submit"
-              className="flex flex-1 h-[52px] justify-center items-center gap-8 rounded-lg focus:ring-purple-dark focus:ring-2 bg-blue-dark focus:outline-none hover:bg-blue transition-colors"
-            >
-              <p className="text-gray-200 font-bold text-sm">Criar</p>
-            </button>
-          </form>
+          <Form />
           {/* <div className="flex flex-col mt-16">
             <div className="flex items-center justify-between mb-6">
               <div className="flex gap-2 items-center">
