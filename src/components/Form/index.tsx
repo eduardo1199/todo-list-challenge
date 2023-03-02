ca54e7cab0c445da59029e8d3917ba1f4fa88ff0
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-02 12:24:49 -0300

    integration validate form with useForm and zod

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,10 +1,31 @@
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const taskFormSchema = z.object({
+  query: z.string(),
+})
+
+type TaskFormData = z.infer<typeof taskFormSchema>
+
 export function Form() {
+  const { register, handleSubmit } = useForm<TaskFormData>({
+    resolver: zodResolver(taskFormSchema),
+  })
+
+  function handleCreateNewTask(data: TaskFormData) {
+    console.log(data)
+  }
+
   return (
-    <form className="mt-[-27px] flex gap-2">
+    <form
+      className="mt-[-27px] flex gap-2"
+      onSubmit={handleSubmit(handleCreateNewTask)}
+    >
       <input
-        type="text"
         className="bg-gray-500 rounded-lg w-[638px] p-4 h-[54px] border border-gray-700 text-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-dark"
         placeholder="Adicione uma nova tarefa"
+        {...register('query')}
       />
       <button
         type="submit"
